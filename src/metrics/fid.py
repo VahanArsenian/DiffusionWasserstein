f638513dbeebee476162a8a71bf51cbcfeb0a327
Vahan Arsenyan <varsenyan@servicetitan.com>
2025-05-01 15:45:29 +0000

    added cifar10 for comparison

--- src/metrics/fid.py
+++ src/metrics/fid.py
@@ -7,7 +7,7 @@ class FIDScore:
 
     def __init__(self, device=0):
         self.device = device
-        self.fid = FrechetInceptionDistance(normalize=False, reset_real_features=False).to(0)
+        self.fid = FrechetInceptionDistance(normalize=False).to(0)
 
     @classmethod
     def preprocess_image(cls, image):
@@ -18,9 +18,12 @@ class FIDScore:
     def update(self, image_set_left, image_set_right):
         self.fid.update(image_set_left, real=True)
         self.fid.update(image_set_right, real=False)
-
+        
     def compute(self):
-        return (self.fid.compute())
+        res = (self.fid.compute())
+        print("Cache reset initiated")
+        self.fid.reset()
+        return res
 
     def load_and_process_files(self, file_path_left, file_path_right):
         image_left = np.load(file_path_left)
@@ -40,7 +43,15 @@ class FIDScore:
 
 if __name__ == "__main__":
     fid = FIDScore()
-    fid.load_and_process_files("src/data/noisy_score_images/generated_images.npz", 
-                            "src/data/standard_score_images/generated_images.npz")
     
-    print(f"FID: {fid.compute()}")
+    fid.load_and_process_files("src/data/cifar10_sample/generated_images.npz", 
+                             "src/data/standard_score_images/generated_images.npz")
+    print(f"FID between true score and CIFAR10: {fid.compute()}")    
+    
+    fid.load_and_process_files("src/data/noisy_score_images/generated_images.npz", 
+                             "src/data/cifar10_sample/generated_images.npz")
+    print(f"FID between noisy score and CIFAR10: {fid.compute()}")    
+    
+    fid.load_and_process_files("src/data/noisy_score_images/generated_images.npz", 
+                             "src/data/standard_score_images/generated_images.npz")
+    print(f"FID between noisy score and true score: {fid.compute()}")
